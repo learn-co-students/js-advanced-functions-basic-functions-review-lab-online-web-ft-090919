19f45a7e17b66fb50290f3b77fcedc5ab5028394
Matthew Ream <matthewtimothyream@gmail.com>
2020-01-11 14:10:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun()
+
+function mondayWork(activity='go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork()
+
+function wrapAdjective(flair='*') {
+    return function(param='special') {
+        return `You are ${flair}${param}${flair}!`
+    }
+}
+
+wrapAdjective()()
+
+const Calculator = {
+    add: function add(a, b) {
+        return a + b
+    },
+    
+    subtract: function subtract(a, b) {
+        return a - b
+    },
+    
+    multiply: function multiply(a, b) {
+        return a * b
+    },
+    
+    divide: function divide(a, b) {
+        return a / b
+    }
+} 
+
+function actionApplyer(integer, array) {
+    if (array.size == 0) {
+        return integer
+    }
+    let result = integer
+    array.forEach(op => result = op(result))
+    return result
+}
